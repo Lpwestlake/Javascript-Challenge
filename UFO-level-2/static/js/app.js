c916bfd4e4e3bcc93abbc0cae06fd96616413e94
Lpwestlake <57381750+Lpwestlake@users.noreply.github.com>
2020-03-21 18:00:51 -0500

    final submit

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,8 +1,10 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-// console.log(tableData)
+// create variable to hold filered date for multiple filters and a list of the active fileters
+var filteredData = [];
+var isFiltered = false;
+var activeFilters = []
 
 // create a variable for the selection of the table body using D3
 var tbody = d3.select("tbody");
@@ -24,7 +26,7 @@ tableData.forEach((tableRender) => {
 // date input field code:
 
 // define the date filter button
-var button = d3.select("#date-btn");
+var button = d3.select("#filter-btn");
 
 button.on("click", (click) => {
     // select the input element and get the html id
@@ -34,46 +36,86 @@ button.on("click", (click) => {
     var inputValue = inputDate.property("value");
     console.log(inputValue);
 
-    // filter the table data by datetime to match input date
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    // if else statment to check if data was previously filtered
+    if (!isFiltered) {
+        // filter the table data by datetime to match input date
+        filteredData = tableData.filter(sighting =>
+            sighting.datetime === inputValue ||
+            sighting.city === inputValue ||
+            sighting.state === inputValue ||
+            sighting.country === inputValue ||
+            sighting.shape === inputValue);
+        console.log(filteredData);
 
-    console.log(filteredData);
+        // remove any previous listed items within the table body
+        tbody.html("");
 
-    // remove any previous listed items within the table body
-    tbody.html("");
+        filteredData.forEach((tableRender) => {
+            var row = tbody.append("tr");
 
-    filteredData.forEach((tableRender) => {
-        var row = tbody.append("tr");
+            // log each value with object.entries function
+            Object.entries(tableRender).forEach(([key, value]) => {
 
-        // use `Object.entries` to log each value
-        Object.entries(tableRender).forEach(([key, value]) => {
-
-            // append a cell to the row for each value
-            var cell = row.append("td");
-            cell.text(value);
+                // append a cell to the row for each value
+                var cell = row.append("td");
+                cell.text(value);
+            });
         });
+    } else {
+        // filter the table data by datetime to match input date
+        filteredData = filteredData.filter(sighting =>
+            sighting.datetime === inputValue ||
+            sighting.city === inputValue ||
+            sighting.state === inputValue ||
+            sighting.country === inputValue ||
+            sighting.shape === inputValue);
+        console.log(filteredData);
+
+        // remove any previous listed items within the table body
+        tbody.html("");
+
+        filteredData.forEach((tableRender) => {
+            var row = tbody.append("tr");
+
+            // log each value with object.entries function
+            Object.entries(tableRender).forEach(([key, value]) => {
+
+                // append a cell to the row for each value
+                var cell = row.append("td");
+                cell.text(value);
+            });
+        });
+    }
+    // setting isFiltered back to true
+    isFiltered = true;
+    // adding the addtional inputValues to the activeFilter array
+    activeFilters.push(inputValue);
+    console.log(activeFilters);
+
+    // clear out filtered list so it doesn't keep appending to old list
+    d3.select("#filterlist").remove();
+
+    // using D3 select the dom element and append items to list of active filters
+    var showFilters = d3.select("#activefilters")
+    showFilters.append("ul")
+        .attr("id", "filterlist")
+    activeFilters.forEach((filter) => {
+        d3.select("#filterlist")
+            .append("li")
+            .attr("class", "filter")
+            .text(filter);
     });
 });
 
-var button = d3.select("#city-btn");
+// creating a button to clear the filters and re-render the original table
+var clearButton = d3.select("#clear-btn");
 
-button.on("click", (click) => {
-    // select the input element and get the html id
-    var inputCity = d3.select("#city");
+clearButton.on("click", (click) => {
 
-    // get the value property of the input element
-    var inputValue = inputCity.property("value");
-    console.log(inputValue);
-
-    // filter the table data by datetime to match input date
-    var filteredData = tableData.filter(sighting => sighting.city === inputValue);
-
-    console.log(filteredData);
-
-    // remove any previous listed items within the table body
+    // clear table data
     tbody.html("");
 
-    filteredData.forEach((tableRender) => {
+    tableData.forEach((tableRender) => {
         var row = tbody.append("tr");
 
         // use `Object.entries` to log each value
@@ -84,97 +126,9 @@ button.on("click", (click) => {
             cell.text(value);
         });
     });
-});
-
-var button = d3.select("#state-btn");
-
-button.on("click", (click) => {
-    // select the input element and get the html id
-    var inputState = d3.select("#state");
-
-    // get the value property of the input element
-    var inputValue = inputState.property("value");
-    console.log(inputValue);
-
-    // filter the table data by datetime to match input date
-    var filteredData = tableData.filter(sighting => sighting.state === inputValue);
-
-    console.log(filteredData);
-
-    // remove any previous listed items within the table body
-    tbody.html("");
-
-    filteredData.forEach((tableRender) => {
-        var row = tbody.append("tr");
-
-        // use `Object.entries` to log each value
-        Object.entries(tableRender).forEach(([key, value]) => {
-
-            // append a cell to the row for each value
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
-});
-
-var button = d3.select("#country-btn");
-
-button.on("click", (click) => {
-    // select the input element and get the html id
-    var inputCountry = d3.select("#country");
-
-    // get the value property of the input element
-    var inputValue = inputCountry.property("value");
-    console.log(inputValue);
-
-    // filter the table data by datetime to match input date
-    var filteredData = tableData.filter(sighting => sighting.country === inputValue);
-
-    console.log(filteredData);
-
-    // remove any previous listed items within the table body
-    tbody.html("");
-
-    filteredData.forEach((tableRender) => {
-        var row = tbody.append("tr");
-
-        // use `Object.entries` to log each value
-        Object.entries(tableRender).forEach(([key, value]) => {
-
-            // append a cell to the row for each value
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
-});
-
-var button = d3.select("#shape-btn");
-
-button.on("click", (click) => {
-    // select the input element and get the html id
-    var inputShape = d3.select("#shape");
-
-    // get the value property of the input element
-    var inputValue = inputShape.property("value");
-    console.log(inputValue);
-
-    // filter the table data by datetime to match input date
-    var filteredData = tableData.filter(sighting => sighting.shape === inputValue);
-
-    console.log(filteredData);
-
-    // remove any previous listed items within the table body
-    tbody.html("");
-
-    filteredData.forEach((tableRender) => {
-        var row = tbody.append("tr");
-
-        // use `Object.entries` to log each value
-        Object.entries(tableRender).forEach(([key, value]) => {
-
-            // append a cell to the row for each value
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
+    // set all arrays back to empty and remove filter list
+    filteredData = [];
+    isFiltered = false;
+    activeFilters = [];
+    d3.select("#filterlist").remove();
 });
